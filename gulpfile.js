8a0d35f8876e79ab60b79cecacda8e617e1f9a0e
Vasiliy <cartario@yandex.ru>
2020-02-03 21:52:12 +0300

    favicon

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,9 @@ gulp.task("copy", function () {
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
     "source/js/**",
-    "source/*.ico"
+    "source/*.ico",
+	"source/robots.txt",
+	"source/Sitemap.txt"
     ], {
       base: "source"
     })
